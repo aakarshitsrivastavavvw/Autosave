39c01e1e59da13f800d85d896033cff803924ee4
aakarshitsrivastavavvw <aakarshit.srivastava.vw@gmail.com>
2025-12-30 16:29:28 +0530

    Create autosave.js

---
+++ js/autosave.js
@@ -0,0 +1,31 @@
+// Auto-save logic
+const AutoSave = (() => {
+  const collectFormData = () => {
+    const data = {};
+    document.querySelectorAll("input").forEach(input => {
+      data[input.id] = input.value;
+    });
+    return data;
+  };
+
+  const saveDraft = () => {
+    const data = collectFormData();
+    Storage.save(data);
+    console.log("Draft auto-saved");
+  };
+
+  const restoreDraft = () => {
+    const data = Storage.load();
+    if (!data) return;
+
+    Object.keys(data).forEach(id => {
+      const el = document.getElementById(id);
+      if (el) el.value = data[id];
+    });
+  };
+
+  return {
+    save: debounce(saveDraft, 800),
+    restore: restoreDraft
+  };
+})();
